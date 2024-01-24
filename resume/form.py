7906ba4a9216a59263b84a23976e999539de7062
tatirob <tsion902@gmail.com>
2024-01-24 19:06:56 +0300

    update

--- resume/form.py
+++ resume/form.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Resume
 
 class UpdateResumeForms(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['upload_resume'].required = True
     class Meta:
         model = Resume
         exclude = ('user' ,)
