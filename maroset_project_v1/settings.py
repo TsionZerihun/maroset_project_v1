3461b42a3cf240620961f98c9f34d8a7a3760719
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2024-01-29 14:53:22 +0300

    Update settings.py

--- maroset_project_v1/settings.py
+++ maroset_project_v1/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
 }
 
 database_url = os.environ.get("DATABASE_URL")
-#DATABASES['default'] = dj_database_url.parse("postgres://maroset:grdV7w3AicrFYfHzsvjm46KM8cGI2LgH@dpg-cmr2gn21hbls73fmr7j0-a.oregon-postgres.render.com/maroset")
-DATABASES['default'] = dj_database_url.parse("database_url")
+#DATABASES['default'] = dj_database_url.parse("postn-postgres.render.com/maroset")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 #postgres://maroset:grdV7w3AicrFYfHzsvjm46KM8cGI2LgH@dpg-cmr2gn21hbls73fmr7j0-a/maroset
 
